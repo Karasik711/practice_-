30abb2296bd78e565ae91a56cb0b1f239c1c22ad
Ksenia <karasevaksenia3644@gmail.com>
2022-09-17 18:40:17 +0300

    задача6

--- task02/Program.cs
+++ task02/Program.cs
@@ -6,6 +6,6 @@
 
 Console.WriteLine("Введите число : ");
 int a = Convert.ToInt32(Console.Read());
-    if (a % 2 == 0) Console.WriteLine("Четное");
-    else Console.WriteLine("Нечетное");
+    if (a % 2 == 0) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
     
