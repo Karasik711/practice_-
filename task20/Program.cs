70a034a2eb062967e8c004ef72e47b488071da22
Ksenia <karasevaksenia3644@gmail.com>
2022-10-09 11:50:31 +0300

    задача 52

---
+++ task20/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+Console.WriteLine("Среднее арифметическое:");
+double[] array2DColumnsAverage = FindColumnsAverage(array2D);
+PrintDoubleArray(array2DColumnsAverage);
+
+double[] FindColumnsAverage(int[,] matrix)
+{
+    double[] columnsAverage = new double[matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        int sumElementsInColumn = default;
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            sumElementsInColumn += matrix[j, i];
+        }
+        columnsAverage[i] = Math.Round((Convert.ToDouble(sumElementsInColumn) / matrix.GetLength(0)), 2);
+    }
+    return columnsAverage;
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("   |");
+    }
+}
+
+void PrintDoubleArray(double[] array)
+{
+    Console.Write("|");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i],4}; ");
+        else Console.Write($"{array[i],4}");
+    }
+    Console.WriteLine("   |");
+}
