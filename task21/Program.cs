587a541156e7aa5de16c58754a9ddeda1cc2ed7d
Ksenia <karasevaksenia3644@gmail.com>
2022-10-15 15:17:39 +0300

    задача 54

---
+++ task21/Program.cs
@@ -0,0 +1,83 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки 
+// двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+int[] arrEx = new int[] { 1, 2, 5, 8, 4 };
+BubbleSort(arrEx);
+
+Console.WriteLine();
+FromHiToLowInRow(array2D);
+PrintMatrix(array2D);
+
+void FromHiToLowInRow(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int[] rowArr = new int[matrix.GetLength(1)];
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            rowArr[j] = matrix[i, j];
+        }
+        BubbleSort(rowArr);
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            matrix[i, k] = rowArr[k];
+        }
+    }
+}
+
+int[] BubbleSort(int[] arr)
+{
+    int temp;
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        for (int j = 0; j < arr.Length - i - 1; j++)
+        {
+            if (arr[j + 1] > arr[j])
+            {
+                temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("   |");
+    }
+}
